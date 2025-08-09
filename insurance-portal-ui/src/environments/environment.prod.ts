6e28ff1f7b29e39f7d388bdbaab09163e07164eb
Carlos Hernandez <caradrherjr@hotmail.com>
2025-08-09 02:26:52 -0700

    redo

--- insurance-portal-ui/src/environments/environment.prod.ts
+++ insurance-portal-ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tv3q9m9k6n.us-east-1.awsapprunner.com',
-  signalRUrl: 'https://tv3q9m9k6n.us-east-1.awsapprunner.com/claimHub',
+  apiUrl: '${API_URL}',
+  signalRUrl: '${API_URL}/claimHub',
 };
